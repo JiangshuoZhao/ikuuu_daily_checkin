a1932c381074c3e173ba9d6c7c0a59afb9bb0a29
JiangshuoZhao <1787076149@qq.com>
2026-07-05 12:56:28 +0800

    Fix ikuuu checkin flow

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import requests, json, re, os
+import requests, json, os
 
 session = requests.session()
 # 配置用户名（一般是邮箱）
@@ -7,42 +7,55 @@ email = os.environ.get('EMAIL')
 passwd = os.environ.get('PASSWD')
 # server酱
 SCKEY = os.environ.get('SCKEY')
+COOKIE = os.environ.get('IKUUU_COOKIE') or os.environ.get('COOKIE')
 
 ## 域名经常出问题
-domain_name = 'https://ikuuu.pw/'
+domain_name = os.environ.get('DOMAIN_NAME', 'https://ikuuu.org/').rstrip('/') + '/'
 login_url = domain_name + 'auth/login'
 check_url = domain_name + 'user/checkin'
 info_url = domain_name + 'user/profile'
 
 header = {
-        'origin': domain_name,
+        'origin': domain_name.rstrip('/'),
+        'referer': login_url,
         'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
 }
 data = {
         'email': email,
         'passwd': passwd
 }
+
+def push(content):
+    if SCKEY:
+        push_url = 'https://sctapi.ftqq.com/{}.send?title=ikuuu自动签到任务提示&desp={}'.format(SCKEY, content)
+        requests.post(url=push_url, timeout=20)
+        print('推送成功')
+
+def response_json(response):
+    response.raise_for_status()
+    return response.json()
+
 try:
-    print('进行登录...')
-    response = json.loads(session.post(url=login_url,headers=header,data=data).text)
-    print("----域名正常-----")
-    print(response['msg'])
-    # 获取账号名称
-    info_html = session.get(url=info_url,headers=header).text
-#     info = "".join(re.findall('<span class="user-name text-bold-600">(.*?)</span>', info_html, re.S))
-#     print(info)
+    if COOKIE:
+        print('使用 COOKIE 进行签到...')
+        header['cookie'] = COOKIE
+    else:
+        print('进行登录...')
+        response = response_json(session.post(url=login_url,headers=header,data=data,timeout=20))
+        print("----域名正常-----")
+        print(response['msg'])
+        if response.get('ret') != 1:
+            raise RuntimeError(response['msg'])
+        # 获取账号名称
+        session.get(url=info_url,headers=header,timeout=20)
     # 进行签到
-    result = json.loads(session.post(url=check_url,headers=header).text)
+    result = response_json(session.post(url=check_url,headers=header,timeout=20))
     print(result['msg'])
     content = result['msg']
     # 进行推送
-    if SCKEY != '':
-        push_url = 'https://sctapi.ftqq.com/{}.send?title=ikuuu自动签到任务提示&desp={}'.format(SCKEY, content)
-        requests.post(url=push_url)
-        print('推送成功')
-except:
-    content = '签到失败'
+    push(content)
+except Exception as exc:
+    content = '签到失败: {}'.format(exc)
     print(content)
-    if SCKEY != '':
-        push_url = 'https://sctapi.ftqq.com/{}.send?title=ikuuu自动签到任务提示&desp={}'.format(SCKEY, content)
-        requests.post(url=push_url)
+    push(content)
+    raise SystemExit(1)
